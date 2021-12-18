30d41ecb99938a5fa01afd6f01d19f79fa263eda
Maxence Rondot <maxence.rondot@hotmail.fr>
2021-12-18 15:46:32 +0100

    First commit

--- app/src/main/java/com/example/app_service/client/ListRdv.java
+++ app/src/main/java/com/example/app_service/client/ListRdv.java
@@ -1,14 +1,16 @@
-package com.example.app_service;
+package com.example.app_service.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.app_service.R;
+
+public class ListRdv extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_rdv);
     }
 }
